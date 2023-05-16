ecea068eca7f99e738af4c49102b26c657eff52a
astrosinifinitos <stroncio1981@gmail.com>
2023-05-16 19:20:37 +0200

    Dragger class

---
+++ src/const.py
@@ -0,0 +1,9 @@
+# Dimensiones
+
+WIDTH = 800
+HEIGHT = 800
+
+# Dimensiones del tablero
+ROWS = 8
+COLS = 8
+SQSIZE = WIDTH // COLS
---
+++ src/main.py
@@ -0,0 +1,63 @@
+import pygame
+import sys
+
+from const import *
+from game import Game
+
+
+class Main:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Ajedrez')
+        self.game = Game()
+
+    def mainloop(self):
+
+        screen = self.screen
+        game = self.game
+        board = self.game.board
+        dragger = self.game.dragger
+
+        while True:
+            self.game.show_bg(screen)
+            game.show_pieces(screen)
+
+            if dragger.dragging:
+                dragger.update_blit(screen)
+            for event in pygame.event.get():
+
+                # hacer click
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
+
+                    # Si la casilla clicada tiene una pieza
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+
+                # mouse motion
+                elif event.type == pygame.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_pieces(screen)
+                        dragger.update_blit(screen)
+
+                # no hacer click
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.undrag_piece()
+                # salir de la app
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+
+
+main = Main()
+main.mainloop()
